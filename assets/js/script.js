0b84cb417042c0a8a11e3d92f2cb98aa9398c10f
Valentina-lan <85942879+Valentina-lan@users.noreply.github.com>
2021-07-15 16:45:08 +0700

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var element = document.getElementById('random'); 
 setInterval(function(){
-    element.innerHTML = Math.floor((Math.random()*20)+1);   
+    element.innerHTML = Math.floor((Math.random()*13)+1);   
 }, 9000);
 
 
